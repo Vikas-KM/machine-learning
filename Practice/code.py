0933cca1f3b3e9584c2e4c7b5426891e60a80416
VikasKM <vikki.km@gmail.com>
2020-06-01 16:24:46 +0530

    breast cancer data and boston data exploring

---
+++ Practice/code.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+        
+import pandas as pd
+df = pd.read_csv('./hrdata.csv')
+columns = list(df.columns)
+
+
